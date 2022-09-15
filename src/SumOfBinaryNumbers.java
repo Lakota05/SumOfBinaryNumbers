a187382526c0166c73b5056a903235c1857478e5
Victor Fortunato <lakotaclip@gmail.com>
2022-09-15 09:57:19 -0600

    first commit

---
+++ src/SumOfBinaryNumbers.java
@@ -0,0 +1,17 @@
+
+public class SumOfBinaryNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a = 10;
+		int b = 11;
+		
+		
+		String binaryString = Integer.toBinaryString(a + b);
+		System.out.println(binaryString);
+
+	}
+
+}
